253454f6dc4a7931852f8d7114e88b4f12a2c4d6
James Yoo <jyoo980@gmail.com>
2023-11-12 10:56:36 -0800

    Adding annotated files (two bugs)

--- src/main/java/io/github/fvarrui/javapackager/packagers/GenerateDmg.java
+++ src/main/java/io/github/fvarrui/javapackager/packagers/GenerateDmg.java
@@ -41,6 +41,7 @@ public class GenerateDmg extends ArtifactGenerator<MacPackager> {
 	}
 	
 	@Override
+  @SuppressWarnings("method.invocation") // bug : get() on possibly-empty.
 	protected File doApply(MacPackager packager) throws Exception {
 
 		File appFolder = packager.getAppFolder();
